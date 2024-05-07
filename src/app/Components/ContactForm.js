943ca3223c9b78ed0be10bc79ca5bd55eddbf8e5
Rana5539 <wa852094@gmail.com>
2024-05-08 03:43:47 +0500

    contact text fields

--- src/app/Components/ContactForm.js
+++ src/app/Components/ContactForm.js
@@ -1,22 +1,38 @@
-import { Box } from '@mui/material'
+'use client'
+import { Box, Typography } from '@mui/material'
 import React from 'react'
-
+import TextField from '@mui/material/TextField';
 function ContactForm() {
   return (
     <>
      <Box sx={{position: 'absolute',
-  top: '50%',
+  top: '55%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
   width: '65vh',
   height: '70vh',
-  backgroundColor: 'rgba(255, 255, 255, 0.25)',
+  backgroundColor: 'rgba(255, 255, 255, 0.15)',
   boxShadow: '0 8px 32px 0 rgba(31, 38, 135, 0.37)',
   borderRadius: '10px',
   border: '1px solid rgba(255, 255, 255, 0.18)',
-  backdropFilter: 'blur(10px)',
-  WebkitBackdropFilter: 'blur(10px)',
-  }}></Box> 
+  backdropFilter: 'blur(5px)',
+  WebkitBackdropFilter: 'blur(5px)',
+  textAlign:'center',
+  alignItems:'center',
+  justifyContent:'center',
+  }}>
+    <Typography sx={{mt:2, color:'#87CEEB', fontSize:'25px', fontFamily:'Poppins', }}>  Contact Us</Typography>
+    <TextField  type='text' placeholder='Name' id="fullWidth"  sx={{mt:4, width:'90%', border:'1px solid #87CEEB','& input::placeholder': {
+      color: 'white', 
+      fontFamily: 'Poppins',
+      fontSize:'17px',
+          },}}/>
+    <TextField  type='email' placeholder='Email' id="fullWidth"  sx={{mt:4, width:'90%', border:'1px solid #87CEEB','& input::placeholder': {
+      color: 'white', 
+      fontFamily: 'Poppins',
+      fontSize:'17px'
+    },}}/>
+    </Box> 
     </>
   )
 }
